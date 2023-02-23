b16eefba6d1c929940e80cc7b8b58876672e63f2
Magnus Bieneck <magnus@bieneck.eu>
2023-02-23 21:45:38 +0100

    Fix setup.py classifiers
    There was a missing comma, so Python interpreted it as a multi-line string.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)"
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: POSIX :: Linux",
     ],
     test_suite = 'tests',
